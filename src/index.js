92ca85679d4d98dc8851e15dc4fa10a89ecef838
martynenkovlad <108723639+martynenkovlad@users.noreply.github.com>
2023-08-29 16:30:26 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//Задание 1
+
+var number = prompt ("Введите число!");
+
+if (number % 3 === 0 && number % 5 === 0) {
+  console.log("FizzBuzz");
+} else if (number % 3 === 0) {
+  console.log("Fizz");
+} else if (number % 5 === 0) {
+  console.log("Buzz");
+}
+
+// Задание 2
+
+let inputYear = parseInt(prompt("Введите год:"));
+
+if ((inputYear % 4 === 0 && inputYear % 100 !== 0) || inputYear % 400 === 0) {
+    console.log(inputYear + " есть высокосным годом.");
+} else {
+    console.log(inputYear + " не есь высокосным годом.");
+}
